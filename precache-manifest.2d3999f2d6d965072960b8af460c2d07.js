6c797a67b105f2a28a23c95dec7c89a7b6212160
Bartosz Kaminski <Bakaminski@wp.pl>
2019-11-05 19:04:45 +0100

    Updates

---
+++ precache-manifest.2d3999f2d6d965072960b8af460c2d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5601724ea66ec2918cbdc39b4b81db2",
+    "url": "/movies-tvSeries-database/index.html"
+  },
+  {
+    "revision": "e95b70ebbc62bca7eefb",
+    "url": "/movies-tvSeries-database/static/css/main.cd5c2d7f.chunk.css"
+  },
+  {
+    "revision": "69f46291cbc688c4e326",
+    "url": "/movies-tvSeries-database/static/js/2.93784754.chunk.js"
+  },
+  {
+    "revision": "e95b70ebbc62bca7eefb",
+    "url": "/movies-tvSeries-database/static/js/main.beb03ed7.chunk.js"
+  },
+  {
+    "revision": "4095f1ae8edfa6ff00e4",
+    "url": "/movies-tvSeries-database/static/js/runtime-main.6c229c05.js"
+  }
+]);
